d76c537af708cda7c42bae73ed69cee13897fcd3
Kevin Tsai <kevintsai@Kevins-MBP-3.fios-router.home>
2019-08-09 12:22:23 -0400

    added filters reducers

---
+++ src/components/TransactionFilters.js
@@ -0,0 +1,17 @@
+import React, { useState, useContext } from 'react';
+import TransactionsContext from '../context/transactions-context';
+
+const TransactionFilters = (props) => {
+  return (
+    <div className="filters-container">
+      <select
+        className="select"
+        value={}
+      >
+        
+      </select>
+    </div>
+  );
+};
+
+export { TransactionFilters as default }
--- src/components/TransactionsApp.js
+++ src/components/TransactionsApp.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import '../styles/App.scss';
+import React, { useReducer } from 'react';
+import '../styles/transactions.scss';
 import TransactionList from '../components/TransactionList';
 import TransactionsContext from '../context/transactions-context';
+import filtersReducer from '../reducers/filters';
 import mockData from '../mock-data/mock-data';
 
 function TransactionsApp() {
 
+  const [ filters, dispatch ] = useReducer(filtersReducer, []);
+
   const transactions = mockData;
 
+  // useEffect(() => {
+  //   if (transactions) {
+  //     dispatch({ type: 'POPULATE_TRANSACTIONS', transactions });
+  //   }
+  // }, []);
+
   return (
-    <TransactionsContext.Provider className="app" value={{ transactions }}>
-      <header className="app-header">
-        <h1 className="transctions-title">
-          Transactions
-        </h1>
-      </header>
-      <TransactionList></TransactionList>
+    <TransactionsContext.Provider className="app" value={{ transactions, filters, dispatch }}>
+      <div className="app-container">
+        <header className="app-header">
+          <h1 className="transctions-title">Transactions</h1>
+        </header>
+        <TransactionList></TransactionList>
+      </div>
     </TransactionsContext.Provider>
   );
 }
